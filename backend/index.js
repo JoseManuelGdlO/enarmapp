c996b5fab1cb4f16a3525d9cdb5badf495da97f7
Jose Manuel <josemanuelgarciadela@gmail.com>
2022-10-07 18:14:23 -0500

    Feat: upload images to host

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
-const enarmStundetsRouter = require("./routes/enarmStudents");
+const enarmStundetsRouter = require("./routes/enarmStudents"); 
 
-
-app.use(express.json());
+app.use(express.json({limit: '25mb'}));
 app.use(
   express.urlencoded({
     extended: true,
+    limit: '25mb'
   })
 );
 
--- backend/routes/enarmStudents.js
+++ backend/routes/enarmStudents.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const enarmStudents = require('../services/enarmStudents');
+const fs = require("fs");
 
-/* GET programming languages. */
+const upload = require('../libs/storage');
+
+
 router.get('/', async function(req, res, next) {
   try {
     res.json(await enarmStudents.getMultiple(req.query.page));
+  } catch (err) {
+    console.error(`Error while getting enarm students info `, err.message);
+    next(err);
+  }
+});
+
+router.post('/upload-profile', upload.single('image'), async function(req, res, next) {
+  try {
+
+    const {image} = req.body;
+    const imagenCoverted = fs.writeFileSync("new-path.jpg", image);
+    
+    //upload.single(imagenCoverted);
   } catch (err) {
     console.error(`Error while getting enarm students info `, err.message);
     next(err);
