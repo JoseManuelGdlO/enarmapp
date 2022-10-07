224aa1064618c1e87bbff1618443ef602f6d2a9c
Jose Manuel <josemanuelgarciadela@gmail.com>
2022-10-07 17:20:54 -0500

    feat: change db configs

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,8 @@
 const config = {
     db: {
       /* don't expose password or any sensitive info, done only for demo */
-    //   host: "sql400.main-hosting.eu",
-    //   host: "45.93.101.52",
-    host: "mysql",
+      // host: "sql400.main-hosting.eu",
+      host: "45.93.101.52",
       user: "u900482938_enarmadmin",
       password: "G#r3EhkkK#a",
       database: "u900482938_enarmapp",
