3153d5a315f4e311cad33b108d7abee82dff46b1
AleksDeveloper <44974969+AleksDeveloper@users.noreply.github.com>
2023-08-28 10:50:23 -0600

    Update environment.ts

--- app/enarmapp/src/environments/environment.ts
+++ app/enarmapp/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false
 };
 
-export const API_URL = 'http://3.230.92.106:3000'
+export const API_URL = 'http://54.86.172.9:3000'
 
 /*
  * For easier debugging in development mode, you can import the following file
